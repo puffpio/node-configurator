d4793f8f2aabfa1f435ee70497d81ae9be15fdc9
David Pio <david@puffpio.com>
2011-05-27 23:07:32 +0000

    add test insert route

--- routes.js
+++ routes.js
@@ -2,9 +2,19 @@ exports.register = function (app, db) {
   app.get('/car/:carID', function (req, res) {
     res.send('car ' + req.params.carID);
   });
-  app.get('/testcount', function (req, res) {
+  app.get('/test/count', function (req, res) {
     db.test.count(function (err, count) {
       res.send(count.toString());
+    });
+  });
+  app.get('/test/insert', function (req, res) {
+    var testobject = { foo: 'bar' };
+    db.test.insert(testobject, function (err, records) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(records);
+      }
     });
   });
 }
