c9aa03a12f3bb866798ad12a1f7e30ea231e4451
David Pio <david@puffpio.com>
2011-05-27 19:06:01 +0000

    anonymize

--- site.js
+++ site.js
@@ -1,18 +1,19 @@
-var http = require('http');
-var express = require('express');
+(function () {
+  var http = require('http');
+  var express = require('express');
+  var app = express.createServer();
 
-var app = express.createServer();
+  var site = {
+    configure: function () {
+      this.use(express.bodyParser());
+      this.use(express.cookieParser());
+      //this.use(this.router);
+      this.use(express.static(__dirname + '/_static'));
+    }
+  };
 
-var site = {
-  configure: function () {
-    this.use(express.bodyParser());
-    this.use(express.cookieParser());
-//    this.use(this.router);
-    this.use(express.static(__dirname + '/_static'));
-  }
-};
+  app.configure(site.configure);
+  //site.registerRoutes(app);
 
-app.configure(site.configure);
-//site.registerRoutes(app);
-
-app.listen(3000);
+  app.listen(3000);
+})();
