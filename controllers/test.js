25dee41ba59923a5d86c8748a9ccd3a59ad03c7d
David Pio <david@puffpio.com>
2011-05-27 23:51:14 +0000

    document endpoints

--- controllers/test.js
+++ controllers/test.js
@@ -1,9 +1,11 @@
 exports.register = function (app, db) {
+  // Returns the number of documents in the test collection
   app.get('/test/count', function (req, res) {
     db.test.count(function (err, count) {
       res.send(count.toString());
     });
   });
+  // Inserts stuff into the test collection and returns the document
   app.get('/test/insert', function (req, res) {
     var testobject = { foo: 'bar' };
     db.test.insert(testobject, function (err, records) {
