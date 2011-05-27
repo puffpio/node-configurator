638eb78d5f3d0bf91037ac5bd634d57b0df3487e
David Pio <david@puffpio.com>
2011-05-27 23:13:08 +0000

    refactor controllers

---
+++ controllers/test.js
@@ -0,0 +1,17 @@
+exports.register = function (app, db) {
+  app.get('/test/count', function (req, res) {
+    db.test.count(function (err, count) {
+      res.send(count.toString());
+    });
+  });
+  app.get('/test/insert', function (req, res) {
+    var testobject = { foo: 'bar' };
+    db.test.insert(testobject, function (err, records) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(records);
+      }
+    });
+  });
+}
