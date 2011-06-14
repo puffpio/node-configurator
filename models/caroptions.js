780981064c2599c79c743dec6cc9b1bc89ce889a
David Pio <david@puffpio.com>
2011-06-14 05:21:44 +0000

    render car options

--- models/caroptions.js
+++ models/caroptions.js
@@ -1,18 +1,26 @@
 // factory for a new car option
-exports.carOption = function (name, description, price, isSelected) {
+exports.carOption = function (id, name, description, price, isSelected) {
   return {
+    id : id === undefined ? '' : id,
     name: name === undefined ? '' : name,
     description: description === undefined ? '' : description,
     price: price === undefined ? 0 : price,
-    isSelected: isSelected === undefined ? false : isSelected
+    isSelected: isSelected === undefined ? false : isSelected,
+    isCarOption: true
   };
 };
 
 // factory for a grouping of car options
-exports.carOptionGroup = function(name, description, options) {
+exports.carOptionGroup = function(id, name, description, options) {
+  for (var i = 0; i < options.length; i++) {
+    options[i]['groupId'] = id;
+  }
+
   return {
+    id: id === undefined ? '' : id,
     name: name === undefined ? '' : name,
     description: description === undefined ? '' : description,
-    options: options === undefined ? [] : options
+    groupOptions: options === undefined ? [] : options,
+    isCarOptionGroup: true
   };
 };
--- models/cars.js
+++ models/cars.js
@@ -3,20 +3,27 @@ var co = require('./caroptions');
 exports.canyonero = {
   name: 'Canyonero',
   description: 'Go anywhere',
-  colorOptionGroup: co.carOptionGroup(
-    'Colors',
-    'Car Colors',
-    [
-      co.carOption('Black', 'Black', 0, false),
-      co.carOption('White', 'White', 0, false),
-      co.carOption('Silver', 'Silver', 0, false),
-      co.carOption('Yellow', 'Special Yellow', 500, false)
-    ]
-  ),
-  navigation: co.carOption('Navigation', 'Never get lost!', 1500, false)
+  options: [
+    co.carOptionGroup(
+      'colors',
+      'Colors',
+      'Car Colors',
+      [
+        co.carOption('black', 'Black', 'Black', 0, false),
+        co.carOption('white', 'White', 'White', 0, false),
+        co.carOption('silver', 'Silver', 'Silver', 0, false),
+        co.carOption('yellow', 'Yellow', 'Special Yellow', 500, false)
+      ]
+    ),
+    co.carOption('navi', 'Navigation', 'Never get lost!', 1500, false)
+  ]
 };
 
 exports.t9000 = {
   name: 'T9000',
-  description: 'its metal'
+  description: 'it\'s metal',
+  options: [
+    co.carOption('navi', 'Navigation', 'Never get lost!', 1500, false),
+    co.carOption('sportpkg', 'Sport Package', 'Makes it easier to catch John Conner', 3000, false)
+  ]
 };
